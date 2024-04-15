58cbf3345809c9a201c89f6aef9e1dda3acbd6bd
Michel Manarin <michelmanarin@gmail.com>
2024-04-15 10:29:00 -0300

    fix: add log

--- src/presentation/components/flows/builder/builder.tsx
+++ src/presentation/components/flows/builder/builder.tsx
@@ -90,21 +90,20 @@ const Player: React.FC = () => {
       data: payload
     }
 
-    axios.request(config)
-      .then((response) => {
-        const detectionsWithTime = response?.data?.map((detection) => ({
-          ...detection,
-          currentTime: currentTime
-        }))
+    const response = await axios.request(config)
 
-        setDetections((prevDetections) => [
-          ...prevDetections,
-          ...detectionsWithTime
-        ])
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    console.log('response >> ', response)
+
+    const detectionsWithTime = response?.data?.map((detection) => ({
+      ...detection,
+      currentTime: currentTime
+    }))
+    console.log('detectionsWithTime >> ', detectionsWithTime)
+
+    setDetections((prevDetections) => [
+      ...prevDetections,
+      ...detectionsWithTime
+    ])
   }
 
   const playVideo = (): void => {
@@ -131,10 +130,20 @@ const Player: React.FC = () => {
   }
 
   return (
-    <div className='App'>
-      <div>
-        <input type='file' accept='video/mp4' onChange={handleFileChange} />
-
+    <div className='flex flex-col justify-center items-center h-screen py-4 w-full'>
+      <div className="mb-4">
+        <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50">
+          <div className="flex flex-col items-center justify-center pt-5 pb-6">
+            <svg className="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+            </svg>
+            <p className="mb-2 text-sm text-gray-500"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+            <p className="text-xs text-gray-500">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+          </div>
+          <input id="dropzone-file" type="file" accept='video/mp4' onChange={handleFileChange} className="hidden" />
+        </label>
+      </div>
+      <div className="mb-4">
         {videoFile && (
           <div className='outsideWrapper'>
             <div className='insideWrapper'>
@@ -154,8 +163,10 @@ const Player: React.FC = () => {
           </div>
         )}
       </div>
-      <button onClick={async (): Promise<void> => await processFrames()}>Computar frames</button>
-      <button onClick={(): void => playVideo()}>Resultado</button>
+      <div>
+        <button className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" onClick={async (): Promise<void> => await processFrames()}>Computar frames</button>
+        <button onClick={(): void => playVideo()}>Resultado</button>
+      </div>
     </div>
   )
 }
