066a1e07ef32d6891ea0b3721348228bed2f8ef4
Michel Manarin <michelmanarin@gmail.com>
2024-04-15 12:11:59 -0300

    feat: make server more cheaper

--- src/presentation/components/flows/builder/builder.tsx
+++ src/presentation/components/flows/builder/builder.tsx
@@ -6,9 +6,10 @@ import axios from 'axios'
 const Player: React.FC = () => {
   const [canvas, setCanvas] = useState(null)
   const [currentTimeResultVideo, setCurrentTimeResultVideo] = useState(null)
+  const [currentTimePrevius, setCurrentTimePrevius] = useState(null)
 
   const videoRef = useRef(null)
-  const [setUserInput] = useState(null)
+  const [userInput, setUserInput] = useState(null)
 
   const [videoFile, setVideoFile] = useState(null)
   const [detections, setDetections] = useState([])
@@ -33,20 +34,22 @@ const Player: React.FC = () => {
       }
       const filterDetections = detections.filter((detection) => detection.currentTime === currentTimeResultVideo)
 
-      if (canvas) {
-        canvas.clear()
+      // make server cheaper
+      const mustNotConsiderFrame = currentTimeResultVideo === currentTimePrevius || !canvas || (currentTimeResultVideo % 2) !== 0
+      if (mustNotConsiderFrame) {
+        return
       }
-
+      canvas.clear()
       filterDetections.map((detection): void => {
         if (canvas) {
-          const rect = new fabric.Rect({
-            height: detection.box.height,
-            width: detection.box.width,
-            fill: 'transparent',
-            stroke: '#ff0000',
-            strokeWidth: 1,
-            selectable: false
-          })
+          // const rect = new fabric.Rect({
+          //   height: detection.box.height,
+          //   width: detection.box.width,
+          //   fill: 'transparent',
+          //   stroke: '#ff0000',
+          //   strokeWidth: 2,
+          //   selectable: false
+          // })
 
           const text = new fabric.Text(detection.class_name, {
             left: Number(detection.box.left) + Number(detection.box.width) / 2,
@@ -55,9 +58,11 @@ const Player: React.FC = () => {
             selectable: false
           })
 
-          canvas.add(rect, text)
+          canvas.add(text)
         }
       })
+
+      setCurrentTimePrevius(currentTimeResultVideo)
     }
   }, [currentTimeResultVideo])
 
@@ -141,7 +146,7 @@ const Player: React.FC = () => {
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
             </svg>
             <p className="mb-2 text-sm text-gray-500"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-            <p className="text-xs text-gray-500">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+            <p className="text-xs text-gray-500">MP4 (Recommended videos under 10 seconds)</p>
           </div>
           <input id="dropzone-file" type="file" accept='video/mp4' onChange={handleFileChange} className="hidden" />
         </label>
@@ -170,6 +175,15 @@ const Player: React.FC = () => {
           <canvas id='c' width={750} height={500}></canvas>
         </div>
       </div>
+      {!haveDetections && !!userInput && (
+        <div role="status">
+          <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+          <span className="sr-only">Loading results...</span>
+        </div>
+      )}
       <div>
         <button className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" onClick={async (): Promise<void> => await processFrames()}>Analyse frame</button>
         <button className="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" disabled={!haveDetections} onClick={playVideo}>Play with results</button>
